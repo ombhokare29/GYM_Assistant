8c9103703e620d8587732fdc2dc343f4841d695a
Om Bhokare <ombhokare29@gmail.com>
2026-03-12 00:37:42 +0530

    Just fun and created a workout model

--- gymassistant/tracker/admin.py
+++ gymassistant/tracker/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import GymAttendance
+from .models import GymAttendance,Workouts
 admin.site.register(GymAttendance)
+admin.site.register(Workouts)
+
--- gymassistant/tracker/models.py
+++ gymassistant/tracker/models.py
@@ -6,3 +6,8 @@ class GymAttendance(models.Model):
     date = models.DateField(auto_now_add=True)
     went_to_gym = models.BooleanField()
     reason = models.CharField(max_length=200, blank=True)
+
+class Workouts(models.Model):
+    date = models.DateField(auto_now_add=True)
+    did_workout = models.BooleanField()
+    why_not = models.CharField(max_length=500, blank=True)
