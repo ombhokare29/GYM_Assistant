23f623b680dd3daa569a2014f47b61e13966a5ac
Om Bhokare <ombhokare29@gmail.com>
2026-03-14 23:11:49 +0530

    settings changed

--- gymassistant/tracker/models.py
+++ gymassistant/tracker/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 
 class GymAttendance(models.Model):
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(unique=True)
     went_to_gym = models.BooleanField()
-    reason = models.CharField(max_length=200, blank=True)
+    reason = models.CharField(max_length=200, blank=True,null= True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Workouts(models.Model):
     date = models.DateField(auto_now_add=True)
     did_workout = models.BooleanField()
     why_not = models.CharField(max_length=500, blank=True)
+
+
+
+
+def __str__(self):
+    return f"{self.date}, -{'Gym' if self.went_to_gym else 'Skipped'}"
