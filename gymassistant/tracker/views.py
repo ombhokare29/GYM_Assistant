3ce319abf2d4b01a74b6f1d2836c6672061fbbda
Om Bhokare <ombhokare29@gmail.com>
2026-03-15 22:43:17 +0530

    commited

--- gymassistant/tracker/views.py
+++ gymassistant/tracker/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from .models import GymAttendance
+from datetime import date
 
-# Create your views here.
 
 def home(request):
-    return render(request,"tracker/home.html")
+
+    today = date.today()
+
+    if request.method == "POST":
+
+        attendance_value = request.POST.get("attendance")
+
+        went_to_gym = attendance_value == "yes"
+
+        GymAttendance.objects.update_or_create(
+            date=today,
+            defaults={"went_to_gym": went_to_gym}
+        )
+
+    # always executed (GET and POST)
+    today_record = GymAttendance.objects.filter(date=today).first()
+
+    total_workouts = GymAttendance.objects.filter(went_to_gym=True).count()
+
+    context = {
+        "today_record": today_record,
+        "total_workouts": total_workouts,
+    }
+
+    return render(request, "tracker/home.html", context)
